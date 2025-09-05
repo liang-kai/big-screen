05224a919218557f072a71e9196e40a432bc754a
liang-kai <lkaientan@gmail.com>
2025-09-05 18:23:38 +0800

    add pic

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    vue(), 
+    tailwindcss(), 
+    ViteImageOptimizer({
+        /* pass your config */
+      }),],
 })
