8b7f2103c34184e9092ec192c743c4710468602d
lmx <807904746@qq.com>
2025-09-05 22:30:20 +0800

    feat: 二级页面处理

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="detail-page">
-    <h1>详情页面</h1>
-    <p>这是详情页面的内容。</p>
-    <router-link to="/">返回首页</router-link>
+    <!-- <router-link to="/">返回首页</router-link> -->
+    <div class="right"></div>
+    <!-- 左边 -->
+    <div class="left">
+      <!-- 左上-->
+       <div class="left-top"></div>
+       <div class="left-bottom">
+        <div class="title">智能系统全流程覆盖-贷前自动化实时审批</div>
+        <div class="content"></div>
+       </div>
+    </div>
   </div>
 </template>
 
@@ -10,28 +18,42 @@
 // 详情页面的逻辑可以在这里编写
 </script>
 
-<style scoped>
+<style scoped lang="less">
 .detail-page {
-  padding: 20px;
-  text-align: center;
+  position: relative;
+  height: 100%;
+  width: 100%;
+  /* text-align: center; */
+}
+.left {
+  width: 74%;
+  height: 300px;
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  // justify-items: center;
+  &-top {
+    border: 1px solid #f00;
+    width: 200px;
+    height: 100px;
+    text-align: center;
+  }
+  
+  &-bottom {
+    width: 98%;
+    height: 100px;
+    background-color: #0d4779;
+    .title {
+      font-size: 26px;
+      color: rgba(255,255,255, 0.8);
+    }
+  }
 }
 
-h1 {
-  color: #42b883;
-  margin-bottom: 20px;
-}
-
-p {
-  margin-bottom: 20px;
-  font-size: 16px;
-}
-
-a {
-  color: #646cff;
-  text-decoration: none;
-}
-
-a:hover {
-  text-decoration: underline;
+.right {
+  float: right;
+  width: 24%;
+  background-color: #0d4779;
+  height: 100vh;
 }
 </style>
