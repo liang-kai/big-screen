0ea3213a7f667a96aa2c13d31f591d4c4bdb6367
liang-kai <lkaientan@gmail.com>
2025-09-05 18:21:36 +0800

    add pic

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,9 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+     <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
