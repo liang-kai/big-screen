5db3f05ce86d91554184ab3ac6d9ccb49b2c1555
liang-kai <lkaientan@gmail.com>
2025-09-06 20:08:13 +0800

    update

--- mock/user.js
+++ mock/user.js
@@ -1,13 +1,23 @@
 export default [
   {
-    url: '/api/user',
-    method: 'get',
+    url: '/api/rtdm/cxzt-screen',
+    method: 'post',
     response: () => {
       return {
-        code: 0,
+        resultcode: '00000000',
         data: {
-          name: 'mock-user',
-          age: 25
+            result: [
+                {
+                    indicator_name: "当日第一笔进件时间",
+                    indicator_dt: "2025-09-06", 
+                    indicator_value: "2025-09-06 00:21:42",
+                    kpi_rn: "",
+                    mail_city: "",
+                    mail_prov: "",
+                    mode: "1" ,
+                    rpt_dt: "2025-09-06",
+                }
+            ]
         }
       }
     }
--- src/utils.js
+++ src/utils.js
@@ -1,29 +1,65 @@
 // uat环境	http://dsi.uatapps.oa.citicbank.com/dsi-spring-cloud-gateway/dsi-data-api/v1/data/api/rtdm/cxzt-screen 生产环境	http://prod-ocp-gw.dsi.eprod-kzx1.cncb/dsi-data-api/v1/data/api/rtdm/cxzt-screen
 // appid：4p1rAHEm 密钥：MzM2MjEzNWQ5ODA1NGNhNWI2M2VlZDNjZjk2OGQ2NWQ=
 
-const returnData = (indicator_name) => {
-  const resultData = JSON.parse(sessionStorage.RESULT).data.result;
-  console.log(resultData);
-  return resultData.filter((item) => {
-    if (item.indicator_name === indicator_name) {
-      return item.indicator_value;
+import CryptoJS from 'crypto-js'
+import { useFetch } from '@vueuse/core'
+
+const isProd = import.meta.env.PROD;
+let url = isProd ? 'http://prod-ocp-gw.dsi.eprod-kzx1.cncb/dsi-data-api/v1/data/api/rtdm/cxzt-screen' : 'http://dsi.uatapps.oa.citicbank.com/dsi-spring-cloud-gateway/dsi-data-api/v1/data/api/rtdm/cxzt-screen'
+
+const appid = isProd ? '4p1rAHEm' : 'lX6ATEWf'
+const secret = isProd ? 'MzM2MjEzNWQ5ODA1NGNhNWI2M2VlZDNjZjk2OGQ2NWQ=' : 'MzBhYzY0M2RjNjkwNDBlY2E3MzcyNzhkNTAyZTg1OGY='
+
+url = '/api/rtdm/cxzt-screen';
+
+export const getData = async (params) => {
+    const timestamp = new Date().getTime();
+    const s = `appid=${appid}&secret=${secret}&timestamp=${timestamp}`;
+    const sign = CryptoJS.MD5(s).toString().toUpperCase();
+    try {
+        const { data, error, isFetching } = await useFetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'appid': appid,
+                'timestamp': timestamp,
+                'signature': sign
+            },
+            body: JSON.stringify(params)
+        });
+        return { data, error, isFetching };
+    } catch (err) {
+        return { data: null, error: err, isFetching: false };
     }
-  });
-};
-const resultCode = JSON.parse(data.value).data.returnCode === "00000000";
-if (resultCode) {
-  sessionStorage.setItem("RESULT", data.value);
-} else {
-  if (!sessionStorage.RESULT) {
-    sessionStorage.setItem("RESULT", []);
-  }
 }
-// 当日第一笔进件时间
-const firstInputTime = returnData("当日第一笔进件时间");
-console.log(firstInputTime, "firstInputTime");
-const firstInputChannle = resultData("当日第一笔进件渠道");
-console.log(firstInputChannle, "firstInputChannle"); // 当日第一笔申请卡版
-const firstqueryCardType = resultData("当日第一笔申请卡版");
-console.log(firstqueryCardType, "firstqueryCardType");
-const totalInput = resultData("当日累计进件");
-console.log(totalInput, "totalInput");
+
+
+// const returnData = (indicator_name) => {
+//   const resultData = JSON.parse(sessionStorage.RESULT).data.result;
+//   console.log(resultData);
+//   return resultData.filter((item) => {
+//     if (item.indicator_name === indicator_name) {
+//       return item.indicator_value;
+//     }
+//   });
+// };
+// const resultCode = JSON.parse(data.value).data.returnCode === "00000000";
+// if (resultCode) {
+//   sessionStorage.setItem("RESULT", data.value);
+// } else {
+//   if (!sessionStorage.RESULT) {
+//     sessionStorage.setItem("RESULT", []);
+//   }
+// }
+// // 当日第一笔进件时间
+// const firstInputTime = returnData("当日第一笔进件时间");
+// console.log(firstInputTime, "firstInputTime");
+
+// const firstInputChannle = resultData("当日第一笔进件渠道");
+// console.log(firstInputChannle, "firstInputChannle"); // 当日第一笔申请卡版
+
+// const firstqueryCardType = resultData("当日第一笔申请卡版");
+// console.log(firstqueryCardType, "firstqueryCardType");
+
+// const totalInput = resultData("当日累计进件");
+// console.log(totalInput, "totalInput");
