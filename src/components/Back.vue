0e3cc05be1cde5137a2536c941f47e50d730c302
liang-kai <lkaientan@gmail.com>
2025-09-06 14:49:39 +0800

    update

---
+++ src/components/Back.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="back-home" @click="goBack">
+    <img src="@/assets/home/arrow-left.png" alt="首页" class="icon" />
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+const router = useRouter();
+const goBack = () => {
+  router.back();
+};
+</script>
+
+<style scoped>
+.back-home {
+  position: absolute;
+  left: 32px;
+  bottom: 32px;
+  z-index: 1000;
+  cursor: pointer;
+  width: 64px;
+  height: 64px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  opacity: 0.3
+}
+.icon {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+  filter: drop-shadow(0 2px 8px rgba(0,0,0,0.2));
+  transition: transform 0.2s;
+}
+.back-home:hover .icon {
+  transform: scale(1.08);
+}
+</style>
--- src/views/Intel.vue
+++ src/views/Intel.vue
@@ -1,12 +1,19 @@
 <template>
-    <div class="w-full h-full">
+    <div class="w-full h-full relative">
         <img src="../assets/intel/bg.jpg" alt="" class="block object-contain w-full h-full absolute">
-        <div class="absolute bg-amber-200/10 left-[22%] top-[35%] w-[12vw] h-[6vw]"></div>
-        <div class="absolute bg-amber-200/10 left-[38%] top-[56%] w-[12vw] h-[6vw]"></div>
-        <div class="absolute bg-amber-200/10 left-[52%] top-[25%] w-[12vw] h-[5vw]"></div>
+        <div class="dai absolute bg-amber-200/10 left-[22%] top-[35%] w-[12vw] h-[6vw]" @click="goDetail(1)"></div>
+        <div class="dai absolute bg-amber-200/10 left-[38%] top-[56%] w-[12vw] h-[6vw]" @click="goDetail(2)"></div>
+        <div class="dai absolute bg-amber-200/10 left-[52%] top-[25%] w-[12vw] h-[5vw]" @click="goDetail(3)"></div>
+        <Back />
     </div>
 </template>
 <script setup>
 import { ref } from 'vue'
 import { useRouter } from 'vue-router';
+import Back from '@/components/Back.vue';
+
+const router = useRouter();
+function goDetail(tag) {
+    router.push({ name: 'detail', query: { tag } });
+}
 </script>
